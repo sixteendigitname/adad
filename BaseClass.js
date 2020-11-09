eb3ec700bdf27c27eedcf65b09645613b58f782d
sixteendigitname <72978538+sixteendigitname@users.noreply.github.com>
2020-11-09 14:01:07 +0530

    hi
    hello

---
+++ BaseClass.js
@@ -0,0 +1,32 @@
+class BaseClass{
+  constructor(x, y, width, angle){
+  var options = {
+
+"restitution":0.8,
+"density":1,
+"friction":1
+
+  }
+this.body = Bodies.rectangle(x,y,width,height,options);
+this.width = width
+this.height = height
+this.image = loadImage("sprite/base.png")
+World.add(world, this.body);
+  }
+display(){
+var angle = this.body.angle;
+push();
+translate(this.body.position.x, this.body.position.y);
+rotate(angle);
+imageMode(CENTER);
+image(this.image, 0, 0, this.width, this.height)
+pop();
+}
+}
+
+
+
+
+
+
+
